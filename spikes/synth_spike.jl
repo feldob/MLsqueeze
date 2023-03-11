44638086b2a4d5b9d0f3dd491f0e532291a6cea6
Felix Dobslaw <felix.dobslaw@miun.se>
2023-03-11 19:54:15 +0100

    formatted synth data into DataFrame with inputs x1 and x2, and output class.

--- spikes/synth_spike.jl
+++ spikes/synth_spike.jl
@@ -1,32 +1,36 @@
-using Plots
-
+# synthetic boundary described as non-linear function
 synth_func(x) = (x+2)*(x^2-4)
 
 check_synth_valid(x::Number,y::Number) = y > synth_func(x)
 
-x = range(-10, 10, 100_000)
+x = range(-10, 10, 1_000)
 y = synth_func.(x)
+
+using Plots
 
 plot(x, y, xlims=(-10, 10), ylims=(-10,10), label = "boundary")
 
 randinrange(n::Integer) = -10 .+ rand(n) .* 20
 
-x = randinrange(200)
-y = randinrange(200)
+using DataFrames
 
-x1, y1, x2, y2 = [], [], [], []
+npoints = 200
+df = DataFrame()
+df.x1 = randinrange(npoints)
+df.x2 = randinrange(npoints)
 
-for i in eachindex(x)
-    if check_synth_valid(x[i], y[i])
-        push!(x1, x[i])
-        push!(y1, y[i])
-    else
-        push!(x2, x[i])
-        push!(y2, y[i])
-    end
-end
+df.class = zeros(Int, npoints)
+foreach(r -> check_synth_valid(r.x1, r.x2) ? r.class = 1 : nothing, eachrow(df))
 
-plot!(x2,y2, seriestype=:scatter, label = "invalid")
-plot!(x1,y1, seriestype=:scatter, label = "valid")
+gfs = groupby(df, :class)
+plot!(gfs[1].x1,gfs[1].x2, seriestype=:scatter, label = "invalid")
+plot!(gfs[2].x1,gfs[2].x2, seriestype=:scatter, label = "valid")
 
+df
+
+## to print plot
 # png("data/synth_class.png")
+
+## to export to CSV file
+# using CSV
+# CSV.write("data/synth.csv", df)
